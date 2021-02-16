d7e57c6285f9f53d956dd130cf01701229110ebf
Valmisson Grizorte <missongrizorte@gmail.com>
2021-02-16 20:46:26 -0300

    init (create project)

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Dollar - Acompanhe o valor do dólar em tempo real</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
