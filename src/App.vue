072c9b627647262ec97883da841c70111d66e3b2
Valmisson Grizorte <missongrizorte@gmail.com>
2021-02-16 22:22:30 -0300

    feat: add styles and font
    add font Roboto and default style on App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,38 @@ export default defineComponent({
   name: 'App'
 })
 </script>
+
+<style>
+:root {
+  --text: #4A515F;
+  --gray: #6C757D;
+  --bg: #FBFCFE;
+  --green: #35C361;
+  --red: #F1485B;
+}
+
+/* Dark Mode colors */
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    --text: #fff;
+    --gray: #C1CBD4;
+    --bg: #2F354A;
+  }
+}
+
+html {
+  box-sizing: border-box;
+}
+
+*::after,
+*::before {
+  box-sizing: inherit;
+}
+
+body {
+  background-color: var(--bg);
+  color: var(--text);
+  font-family: 'Roboto', sans-serif;
+}
+</style>
